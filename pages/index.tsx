e53e68df54f2b370f53cd0f5d88845aed3af4437
Dhruv Anand <ananddhruv29@gmail.com>
2024-08-19 19:48:00 +0530

    Configured styling and created starting page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
+import { Inter, Raleway } from "next/font/google";
+import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
+const raleway = Raleway({
+	subsets: ["latin"],
+	weight: "600",
+});
 
 export default function Home() {
 	return (
 		<>
-			<h1>
-				<p>Helo</p>
-			</h1>
+			<Head>
+				<title>Chicane</title>
+			</Head>
+			<main className={raleway.className}>
+				<div className="h-screen flex justify-center">
+					<h1 className="content-center text-3xl">Welcome to Chicane!</h1>
+				</div>
+			</main>
 		</>
 	);
 }
