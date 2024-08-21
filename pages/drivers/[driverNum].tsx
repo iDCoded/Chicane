0c7563d5a32544a4c08e62633b0b99b912437928
Dhruv Anand <ananddhruv29@gmail.com>
2024-08-22 00:09:02 +0530

    Dynamically display driver helmet

--- pages/drivers/[driverNum].tsx
+++ pages/drivers/[driverNum].tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { CardTitle } from "@/components/ui/card";
 
 /**
  * Dynamic Driver Page
@@ -9,6 +10,7 @@ export default function DriverDetail() {
 	const router = useRouter();
 	const { driverNum } = router.query;
 	const [driverData, setDriverData] = useState<Driver | null>(null);
+
 	useEffect(() => {
 		if (driverNum) {
 			const fetchDriverData = async () => {
@@ -23,6 +25,16 @@ export default function DriverDetail() {
 		}
 	}, [driverNum]);
 
+	const formatDriverHelmetURL = (driverName: string) => {
+		const baseURL =
+			"https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1024/content/dam/fom-website/manual/Helmets2024/";
+
+		let lastName = driverName.split(" ")[1];
+
+		const URL = `${baseURL}${lastName.toLowerCase()}.png`;
+		return URL;
+	};
+
 	if (!driverData) {
 		return (
 			<>
@@ -35,7 +47,7 @@ export default function DriverDetail() {
 
 	return (
 		<>
-			<div className="h-screen">
+			<div className="h-screen flex flex-row">
 				<div className="driver-image flex flex-col p-12">
 					<Image
 						className="rounded-md shadow-md"
@@ -47,6 +59,30 @@ export default function DriverDetail() {
 					<h3 className="mt-4 pl-2 font-semibold text-3xl">
 						{driverData.name_acronym}
 					</h3>
+				</div>
+				<div
+					className="w-1 h-14 mt-[72px]"
+					style={{ backgroundColor: `#${driverData.team_colour}` }}
+				/>
+				<div className="helmet mt-16">
+					<Image
+						src={formatDriverHelmetURL(driverData.full_name)}
+						alt={driverData.full_name}
+						width={100}
+						height={100}
+					/>
+				</div>
+				<div className="flex flex-row pt-16 ml-6">
+					<div className="flex flex-col">
+						<CardTitle className="font-light text-lg">
+							{/* {parseFirstName(props.name)} */}
+							{driverData.first_name}
+						</CardTitle>
+						<CardTitle className="font-semibold">
+							{/* {parseLastName(props.name)} */}
+							{driverData.last_name}
+						</CardTitle>
+					</div>
 				</div>
 			</div>
 		</>
